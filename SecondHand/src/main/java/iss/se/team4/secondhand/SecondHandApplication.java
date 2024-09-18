eea4bab48188d4eb4455720ed3f50f3b7ab98142
asdsasdfsd <102040474+asdsasdfsd@users.noreply.github.com>
2024-09-18 11:46:13 +0800

    first commit

---
+++ SecondHand/src/main/java/iss/se/team4/secondhand/SecondHandApplication.java
@@ -0,0 +1,15 @@
+package iss.se.team4.secondhand;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SecondHandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandApplication.class, args);
+    }
+
+}
