5eb2cb1cb87728620cb4da172b56ac6b5dcbfee2
Brian7788 <71218337+Brian7788@users.noreply.github.com>
2024-10-09 19:01:08 +0800

    test vue

--- frontend-vue/jest.config.ts
+++ frontend-vue/jest.config.ts
@@ -2,7 +2,13 @@ import type { Config } from 'jest';
 
 const config: Config = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+    testEnvironment: 'jest-environment-jsdom',
+    roots: ['<rootDir>/src'],
+    testMatch: [
+        '**/__tests__/**/*.[jt]s?(x)',
+        '**/?(*.)+(spec|test).[tj]s?(x)',
+    ],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'vue'],
 };
 
 export default config;
