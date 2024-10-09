dee55a9b39b98081edb092ab1bea39c071b5911e
Brian7788 <71218337+Brian7788@users.noreply.github.com>
2024-10-09 19:33:04 +0800

    test vue

--- frontend-vue/jest.config.ts
+++ frontend-vue/jest.config.ts
@@ -8,14 +8,10 @@ const config: Config = {
     roots: ['<rootDir>/src'],
     testMatch: [
         '**/tests/**/*.[jt]s?(x)',
-        '**/?(*.)+(test).[tj]s?(x)',
+        '**/?(*.)+(spec|test).[tj]s?(x)',
     ],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'vue'],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }), // 支持路径别名
-    transform: {
-        '^.+\\.vue$': 'vue-jest',
-        '^.+\\.ts$': 'ts-jest',
-    },
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
 
 export default config;
