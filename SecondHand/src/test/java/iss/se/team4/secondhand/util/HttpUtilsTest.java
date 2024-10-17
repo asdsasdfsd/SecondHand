b8ff2149b7c855f66616cd2202e555bba76d5a95
Brian7788 <71218337+Brian7788@users.noreply.github.com>
2024-10-17 22:30:27 +0800

    test HttpUtilsTest

--- SecondHand/src/test/java/iss/se/team4/secondhand/util/HttpUtilsTest.java
+++ SecondHand/src/test/java/iss/se/team4/secondhand/util/HttpUtilsTest.java
@@ -2,7 +2,6 @@ package iss.se.team4.secondhand.util;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,40 +9,41 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HttpUtilsTest {
 
     @Autowired
     private HttpUtils httpUtils;
 
     @Test
-    void sendGet() throws IOException, InterruptedException {
-        HttpUtils mockedHttpUtils = Mockito.mock(HttpUtils.class);
+    void sendGet() {
         String url = "https://www.google.com";
         Map<String, String> headers = new HashMap<>();
         headers.put("Accept", "application/json");
+        try {
+            String response = httpUtils.sendGet(url, headers);
+            System.out.println(response);
+        } catch (IOException | InterruptedException e) {
+            Assertions.fail();
+        }
 
-        when(mockedHttpUtils.sendGet(url, headers)).thenReturn("Mocked Response");
-
-        String response = mockedHttpUtils.sendGet(url, headers);
-        System.out.println(response);
-        Assertions.assertEquals("Mocked Response", response);
     }
 
     @Test
-    void sendPost() throws IOException, InterruptedException {
-        HttpUtils mockedHttpUtils = Mockito.mock(HttpUtils.class);
+    void sendPost() {
         String url = "https://www.google.com";
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        String jsonBody = "{\"key\":\"value\"}";
+        String jsonBody = "1231";
 
-        when(mockedHttpUtils.sendPost(url, jsonBody, headers)).thenReturn("Mocked Response");
-
-        String response = mockedHttpUtils.sendPost(url, jsonBody, headers);
-        System.out.println(response);
-        Assertions.assertEquals("Mocked Response", response);
+        try {
+            String response = httpUtils.sendPost(url, jsonBody, headers);
+            System.out.println(response);
+        } catch (IOException | InterruptedException e) {
+            Assertions.fail();
+        }
     }
 }
