2fa679e8b28be398e7a078756e2b16d9362e806c
Brian7788 <71218337+Brian7788@users.noreply.github.com>
2024-10-18 18:33:49 +0800

    test user

--- SecondHand/src/test/java/iss/se/team4/secondhand/UserServiceTests/UserServiceImplTest.java
+++ SecondHand/src/test/java/iss/se/team4/secondhand/UserServiceTests/UserServiceImplTest.java
@@ -11,6 +11,7 @@ import iss.se.team4.secondhand.util.JwtUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
