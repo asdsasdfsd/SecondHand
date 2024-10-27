0179c03d195de89c477a7e15e0633b77e401d076
asdsasdfsd <102040474+asdsasdfsd@users.noreply.github.com>
2024-10-28 02:16:14 +0800

    new

--- SecondHand/src/main/java/iss/se/team4/secondhand/config/WebConfig.java
+++ SecondHand/src/main/java/iss/se/team4/secondhand/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://47.236.170.163:31012") 
+                .allowedOrigins("http://localhost:5173", "http://47.236.170.163:31012")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
